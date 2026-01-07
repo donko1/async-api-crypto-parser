169a4fd15ea93389338aa4df571c5d1700a80fe0
donko1 <zalbertic@mail.ru>
2026-01-07 03:51:30 +0300

    Renaming file_path to html_path. Adding json wrtiting in parser_html.py

--- parser/parser_html.py
+++ parser/parser_html.py
@@ -1,28 +1,43 @@
+import json
 from selectolax.parser import HTMLParser
 import os
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(base_dir, "html_cache", f"html_cache.html")
+html_path = os.path.join(base_dir, "html_cache", f"html_cache.html")
+json_path = os.path.join(base_dir, "json_cache", f"json_coins.json")
 
 
-def get_values_from_html_to_dict(filepath=file_path):
-    with open(file_path) as f:
+def get_values_from_html_to_dict(filepath=html_path):
+    with open(html_path) as f:
         html = f.read()
 
     tree = HTMLParser(html)
     trs = tree.css("tr")[2::]
-    for tr in trs:
+
+    out = {}
+
+    for i, tr in enumerate(trs):
         try:
-            print(f"{tr.css('td')[2].css("span")[1].text()}")
-            print(f"{tr.css('td')[2].css("span")[2].text()}")
+            name = f"{tr.css('td')[2].css("span")[1].text()}"
+            ticker = f"{tr.css('td')[2].css("span")[2].text()}"
         except IndexError:
-            print(f"{tr.css('td')[2].css("p")[0].text()}")
-            print(f"{tr.css('td')[2].css("p")[1].text()}")
-        print(f"{tr.css('td')[3].text().replace("$", "").replace(",", ".")}\n")
+            name = f"{tr.css('td')[2].css("p")[0].text()}"
+            ticker = f"{tr.css('td')[2].css("p")[1].text()}"
+        price = f"{tr.css('td')[3].text().replace("$", "").replace(",", ".")}"
+
+        out[ticker] = {"ticker": ticker, "price": price, "name": name, "id": i}
+
+    return out
+
+
+def save_values_to_json(data, filepath=json_path):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
 
 
 def main():
-    print(get_values_from_html_to_dict())
+    save_values_to_json(get_values_from_html_to_dict())
+    print("Successfully completed!")
 
 
 if __name__ == "__main__":
--- parser/parser_site.py
+++ parser/parser_site.py
@@ -5,7 +5,7 @@ import os
 
 main = __name__ == "__main__"
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(base_dir, "html_cache", f"html_cache.html")
+html_path = os.path.join(base_dir, "html_cache", f"html_cache.html")
 
 
 async def get_html_for_top_100():
@@ -17,7 +17,7 @@ async def get_html_for_top_100():
             if main:
                 print("Status:", resp.status)
             html = await resp.text()
-            async with aiofiles.open(file_path, "w") as f:
+            async with aiofiles.open(html_path, "w") as f:
                 await f.write(html)
 
 
