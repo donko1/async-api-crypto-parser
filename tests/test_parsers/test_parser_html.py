ecbd301c1876c9ae0609645d798a61eb4303adca
donko1 <zalbertic@mail.ru>
2026-01-14 18:02:53 +0300

    Adding tests for save_values_to_json

--- tests/test_parsers/test_parser_html.py
+++ tests/test_parsers/test_parser_html.py
@@ -1,6 +1,7 @@
+import json
 from pathlib import Path
 
-from parser.parser_html import get_values_from_html_to_dict
+from parser.parser_html import get_values_from_html_to_dict, save_values_to_json
 
 
 def test_parse_html_to_dict_with_p():
@@ -37,3 +38,15 @@ def test_parse_html_to_dict_combination():
 
     assert "AAPL" in result
     assert result["AAPL"]["name"].lower().startswith("apple")
+
+
+def test_save_values_to_json(tmp_path):
+    file_path = tmp_path / "json.json"
+
+    test_dict = {"test_key": "test_value"}
+    save_values_to_json(data=test_dict, filepath=file_path)
+
+    with open(file_path) as f:
+        d = json.load(f)
+
+    assert d == test_dict
