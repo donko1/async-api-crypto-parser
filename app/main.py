073823149f25acd9c9d4a26c2212d8472ffc44d3
donko1 <zalbertic@mail.ru>
2026-04-28 14:43:06 +0300

    Adding changing settings to fastapi api

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from re import L
 
+from config.settings import SettingsManager
 from fastapi import FastAPI
 
 from services.MarketDataService import MarketDataService
+from core.logger import get_logger
 
 app = FastAPI(
     title="Crypto parser API", version="0.0.1", docs_url="/docs", redoc_url="/redoc"
 )
 
 parser_service = MarketDataService()
+
+logger = get_logger("fastapi main.py")
 
 
 @app.get("/")
@@ -33,5 +37,12 @@ async def status_parser():
     return parser_service.get_status()
 
 
-# @app.post()
-# async def change_env_value():
+@app.post("/change_settings_value")
+async def change_setting_value(key, new_value):
+    settings = SettingsManager()
+    try:
+        settings.set(key, new_value)
+        return {"Message": f"Setting '{key}' updated to '{new_value}'"}
+    except ValueError as e:
+        logger.error(f"Error updating setting: {e}")
+        return {"Key does not exist"}
